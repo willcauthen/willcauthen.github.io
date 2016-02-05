36938f75dd38cd64b73be9cc2cadfce78c368ef4
Will Cauthen <willcauthen50@gmail.com>
2016-02-05 14:58:34 -0800

    tic tac toe game started

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
 console.log("we're in good shape");
+var whoseTurn = "X";
+var hash = [[null, null, null],
+			[null, null, null],
+			[null, null, null]];
+
+$(document).ready(function() {
+	$("div div").click(function () {
+		console.log(this);
+		turn();
+	});
+});
+
+
+function game() {
+
+}
+
+function turn() {
+	if (whoseTurn == "O") {
+		console.log("O's turn");
+		whoseTurn = "X";
+	} else if (whoseTurn == "X") {
+		console.log("X's turn");
+		whoseTurn = "O";
+	} else {
+		whoseTurn = "X";
+	}
+}
