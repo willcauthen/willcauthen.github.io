c17335bad2374fb11e2d5b3f703f72dc0d5d9fc9
Will Cauthen <willcauthen50@gmail.com>
2016-02-06 06:47:56 -0800

    set edge case for when a winner is already determined the game ends

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,61 @@
 console.log("we're in good shape");
 var whoseTurn = "X";
+var winner = false;
 var hash = [[1, 2, 3],
 			[4, 5, 6],
 			[7, 8, 9]];
 
 $(document).ready(function() {
 	$("div div").click(function () {
-		if (this.id){
-			switch (this.id) {
-				case "a1":
-				hash[0][0] = whoseTurn;
-				break;
-				case "a2":
-				hash[0][1] = whoseTurn;
-				break;
-				case "a3":
-				hash[0][2] = whoseTurn;
-				break;
-				case "b1":
-				hash[1][0] = whoseTurn;
-				break;
-				case "b2":
-				hash[1][1] = whoseTurn;
-				break;
-				case "b3":
-				hash[1][2] = whoseTurn;
-				break;
-				case "c1":
-				hash[2][0] = whoseTurn;
-				break;
-				case "c2":
-				hash[2][1] = whoseTurn;
-				break;
-				case "c3":
-				hash[2][2] = whoseTurn;
-				break;
+		if (!winner) {
+			if (this.id){
+					switch (this.id) {
+						case "a1":
+						hash[0][0] = whoseTurn;
+						break;
+						case "a2":
+						hash[0][1] = whoseTurn;
+						break;
+						case "a3":
+						hash[0][2] = whoseTurn;
+						break;
+						case "b1":
+						hash[1][0] = whoseTurn;
+						break;
+						case "b2":
+						hash[1][1] = whoseTurn;
+						break;
+						case "b3":
+						hash[1][2] = whoseTurn;
+						break;
+						case "c1":
+						hash[2][0] = whoseTurn;
+						break;
+						case "c2":
+						hash[2][1] = whoseTurn;
+						break;
+						case "c3":
+						hash[2][2] = whoseTurn;
+						break;
+					}
+					var id = this.id;
+					if (whoseTurn == "X") {
+						var tile = "X" + id;
+						tile = document.getElementById(tile);
+						$(tile).css("z-index", 0);
+						$(this).attr("id", null);
+					} else if (whoseTurn == "O") {
+						var tile = "O" + id;
+						tile = document.getElementById(tile);
+						$(tile).css("z-index", 0);
+						$(this).attr("id", null);
+					}
+					turn();
+					checkWinner();
+				}
+			} else{
+				alert("WE ALREADY HAVE A WINNER, FOOL");
 			}
-			var id = this.id;
-			if (whoseTurn == "X") {
-				var tile = "X" + id;
-				tile = document.getElementById(tile);
-				$(tile).css("z-index", 0);
-				$(this).attr("id", null);
-			} else if (whoseTurn == "O") {
-				var tile = "O" + id;
-				tile = document.getElementById(tile);
-				$(tile).css("z-index", 0);
-				$(this).attr("id", null);
-			}
-			turn();
-			checkWinner();
-		}
 	});
 });
 
@@ -66,26 +71,29 @@ function turn() {
 	}
 }
 function checkWinner() {
-	if 	(
-			//first row
-			((hash[0][0] == hash[0][1]) && (hash[0][1] == hash[0][2])) || 
-			//second row
-			((hash[1][0] == hash[1][1]) && (hash[1][1] == hash[1][2])) || 
-			//third row
-			((hash[2][0] == hash[2][1]) && (hash[2][1] == hash[2][2])) || 
-			// first column
-			((hash[0][0] == hash[1][0]) && (hash[1][0] == hash[2][0])) || 
-			// second column
-			((hash[0][1] == hash[1][1]) && (hash[1][1] == hash[2][1])) || 
-			// third column
-			((hash[0][2] == hash[1][2]) && (hash[1][2] == hash[2][2])) ||
-			//backslash
-			((hash[0][0] == hash[1][1]) && (hash[1][1] == hash[2][2])) || 
-			//forward slash
-			((hash[0][2] == hash[1][1]) && (hash[1][1] == hash[2][0]))
-
-		) {
-		turn();
-		alert(whoseTurn + " is the winner");
-	}
+		if (!winner) {	
+			if 	(
+					//first row
+					((hash[0][0] == hash[0][1]) && (hash[0][1] == hash[0][2])) || 
+					//second row
+					((hash[1][0] == hash[1][1]) && (hash[1][1] == hash[1][2])) || 
+					//third row
+					((hash[2][0] == hash[2][1]) && (hash[2][1] == hash[2][2])) || 
+					// first column
+					((hash[0][0] == hash[1][0]) && (hash[1][0] == hash[2][0])) || 
+					// second column
+					((hash[0][1] == hash[1][1]) && (hash[1][1] == hash[2][1])) || 
+					// third column
+					((hash[0][2] == hash[1][2]) && (hash[1][2] == hash[2][2])) ||
+					//diagonal left to right
+					((hash[0][0] == hash[1][1]) && (hash[1][1] == hash[2][2])) || 
+					// diagonal right to left
+					((hash[0][2] == hash[1][1]) && (hash[1][1] == hash[2][0]))
+		
+				) {
+				turn();
+				alert(whoseTurn + " is the winner");
+				winner = true;
+			}
+		}
 }
