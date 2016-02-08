41314db9735673ca4d944aa98f81efa2eba9b108
Will Cauthen <willcauthen50@gmail.com>
2016-02-08 10:06:01 -0800

    board is in shape and has functioning selector

--- assets/js/connect4.js
+++ assets/js/connect4.js
@@ -1,1 +1,12 @@
 console.log("we're in good shape");
+
+$(document).ready(function () {
+
+	// selector, for UI
+	$("div.col").on("mouseover", function() {
+		console.log("hovering");
+		$(this).css("background", "rgba(50, 0, 0, .3)");
+	}).on("mouseleave", function () {
+		$(this).css("background", "white");
+	});
+});
